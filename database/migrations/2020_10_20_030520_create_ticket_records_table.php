82fe2c3fa62f360ace07cd68865be41f56e16731
yesImNew <ahmedmaazin55@gmail.com>
2020-10-20 08:11:18 +0500

    add ticket records

---
+++ database/migrations/2020_10_20_030520_create_ticket_records_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_records', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('activity_id')->constrained();
+            $table->foreignId('customer_id')->constrained();
+            $table->foreignId('scheduled_event_id')->constrained();
+            $table->integer('no_of_tickets');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_records');
+    }
+}
